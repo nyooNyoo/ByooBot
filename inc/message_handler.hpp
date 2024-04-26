ab6bb6dc502bc07b2707bb712eec2059676541e3
nyooNyoo <nicholasod@proton.me>
2024-04-26 10:47:27 -0400

    Consistent naming

---
+++ inc/message_handler.hpp
@@ -0,0 +1,50 @@
+#ifndef MESSAGE_HANDLER__HPP__
+#define MESSAGE_HANDLER__HPP__
+
+#include <dpp/commandhandler.h>
+#include <dpp/dispatcher.h>
+#include <dpp/dpp.h>
+#include <dpp/snowflake.h>
+
+#include "commands/commands.hpp"
+#include <unordered_map>
+
+typedef std::function<bool(dpp::message_create_t, class dpp::cluster *)>
+    channel_hook;
+
+class message_handler {
+public:
+  std::unordered_map<std::string, command_definition> commands;
+
+  std::unordered_map<dpp::snowflake, channel_hook> hooks;
+
+  std::vector<std::string> prefixes;
+
+  class dpp::cluster *owner;
+
+  dpp::event_handle messages;
+
+public:
+  message_handler(class dpp::cluster *o);
+
+  ~message_handler();
+
+  message_handler &set_owner(class dpp::cluster *o);
+
+  message_handler &add_prefix(const std::string &prefix);
+
+  message_handler &add_command(const std::string &command,
+                               command_handler handler,
+                               const std::string &description = "");
+
+  message_handler &add_hook(const dpp::snowflake &channel_id,
+                            channel_hook channel_hook);
+
+  message_handler &remove_hook(const dpp::snowflake &channel_id);
+
+  void route(const struct dpp::message_create_t &event);
+
+  bool string_has_prefix(std::string &s);
+};
+
+#endif // !MESSAGE_HANDLER__HPP__
--- src/message_handler.cpp
+++ src/message_handler.cpp
@@ -1,4 +1,4 @@
-#include "../inc/messagehandler.hpp"
+#include "../inc/message_handler.hpp"
 #include <dpp/cache.h>
 #include <dpp/cluster.h>
 #include <dpp/commandhandler.h>
@@ -7,7 +7,7 @@
 #include <dpp/snowflake.h>
 #include <dpp/stringops.h>
 
-messagehandler::messagehandler(dpp::cluster *o) : owner(o) {
+message_handler::message_handler(dpp::cluster *o) : owner(o) {
   messages = o->on_message_create([this](const dpp::message_create_t &event) {
     auto found_id = this->hooks.find(event.msg.channel_id);
     if (found_id == this->hooks.end() ||
@@ -17,25 +17,25 @@ messagehandler::messagehandler(dpp::cluster *o) : owner(o) {
   });
 };
 
-messagehandler &messagehandler::set_owner(dpp::cluster *o) {
+message_handler &message_handler::set_owner(dpp::cluster *o) {
   owner = o;
   return *this;
 }
 
-messagehandler::~messagehandler() {
+message_handler::~message_handler() {
   if (messages) {
     owner->on_message_create.detach(messages);
   }
 }
 
-messagehandler &messagehandler::add_prefix(const std::string &prefix) {
+message_handler &message_handler::add_prefix(const std::string &prefix) {
   prefixes.emplace_back(prefix);
   return *this;
 }
 
-messagehandler &messagehandler::add_command(const std::string &command,
-                                            command_handler handler,
-                                            const std::string &description) {
+message_handler &message_handler::add_command(const std::string &command,
+                                              command_handler handler,
+                                              const std::string &description) {
   command_definition i;
   i.function = std::move(handler);
   i.description = description;
@@ -43,18 +43,19 @@ messagehandler &messagehandler::add_command(const std::string &command,
   return *this;
 }
 
-messagehandler &messagehandler::add_hook(const dpp::snowflake &channel_id,
-                                         channel_hook channel_hook) {
+message_handler &message_handler::add_hook(const dpp::snowflake &channel_id,
+                                           channel_hook channel_hook) {
   hooks[channel_id] = channel_hook;
   return *this;
 }
 
-messagehandler &messagehandler::remove_hook(const dpp::snowflake &channel_id) {
+message_handler &
+message_handler::remove_hook(const dpp::snowflake &channel_id) {
   hooks.erase(hooks.find(channel_id));
   return *this;
 }
 
-bool messagehandler::string_has_prefix(std::string &str) {
+bool message_handler::string_has_prefix(std::string &str) {
   for (auto &p : prefixes) {
     size_t prefix_length = dpp::utility::utf8len(p);
     if (dpp::utility::utf8substr(str, 0, prefix_length) == p) {
@@ -65,7 +66,7 @@ bool messagehandler::string_has_prefix(std::string &str) {
   return false;
 }
 
-void messagehandler::route(const struct dpp::message_create_t &event) {
+void message_handler::route(const struct dpp::message_create_t &event) {
   std::string msg_content = event.msg.content;
   if (this->string_has_prefix(msg_content)) {
     std::stringstream ss(msg_content);
