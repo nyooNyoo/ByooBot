79b9b38acd453cb636905c90c6737e3cf600f260
nyooNyoo <nicholasod@proton.me>
2024-04-30 18:34:13 -0400

    Help is a little prettier

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
+#include <cstdlib>
 #include <fstream>
 #include <string>
 
 #include <dpp/commandhandler.h>
 #include <dpp/dpp.h>
+#include <getopt.h>
 #include <unordered_map>
 
 #include "../inc/commands/commands.hpp"
@@ -12,7 +14,34 @@
 
 nlohmann::json config;
 
-int main() {
+int main(int argc, char *argv[]) {
+
+  /* Parse command line arguments */
+  int dev = 0;
+  int index;
+  char arg;
+  struct option longopts[] = {{"dev", no_argument, &dev, 1},
+                              {"help", no_argument, NULL, 'h'}};
+
+  while ((arg = getopt_long_only(argc, argv, "", longopts, &index)) != -1) {
+    switch (arg) {
+    case 0:
+      break;
+    case 'h':
+      std::cout << "Usage: " << argv[0] << " [--help] [--dev]\n";
+      std::cout << "--help:  Prints this menu and closes the program\n";
+      std::cout << "--dev:   Uses the dev bot token and restricts the bot to\n"
+                   "         the channel specified in config.json\n";
+      return EXIT_SUCCESS;
+      break;
+    case '?':
+    default:
+      std::cerr << "Unknown parameter '" << argv[optind - 1] << "'\n";
+      std::cerr << "Use '" << argv[0] << "--help' to print arguments";
+      return EXIT_FAILURE;
+      break;
+    }
+  }
 
   /* Load config file */
   std::ifstream config_stream("../config.json");
@@ -24,7 +53,7 @@ int main() {
       {"help", {help_handler, help_description}}};
 
   /* Start bot */
-  dpp::cluster bot(config["devtoken"],
+  dpp::cluster bot(dev ? config["devtoken"] : config["maintoken"],
                    dpp::i_default_intents | dpp::i_message_content);
   message_handler message_handler(&bot);
 
